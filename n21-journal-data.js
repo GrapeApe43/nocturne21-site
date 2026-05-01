7e67aa26d0730d4c3061fc179bfa7777e6c428c1
GrapeApe43 <aprilferreroart@gmail.com>
2026-04-30 22:38:11 -0400

    Update n21-journal-data.js

--- n21-journal-data.js
+++ n21-journal-data.js
@@ -1,27 +1,41 @@
 const N21_JOURNAL_POSTS = [
 
   {
-    id: "first-entry",
-    title: "Chapter Five Begins",
-    date: "2026-03-31",
-    excerpt: "A little behind-the-scenes look at the beginning of chapter five and what this shift means for the story.",
+    id: "busy-bee",
+    title: "Work and Life",
+    date: "2026-04-30",
+    excerpt: "Big life changes slowed the comic process, but we're getting there!",
     content: `
-      <p>Full post content goes here...</p>
+      <p>Not much to say here. Just wanted to give a quick update. I got a new full time job at the beginning of April. It's kept me super 
+      busy and it's been tough adjusting to the changes. I missed a couple of weeks of posting, but I'm starting to pick up the pace. I know 
+      last month, I had to take some time away to try and work on my coloring process. I think I'm starting to get to a good place with that. 
+      I may keep experimenting with coloring techniques, but for now it's simple, detailed and I can get my coloring done a little quicker.
+      Hopefully, that means I won't have to keep skipping weeks and <i>maybe</i> just maybe I can even get ahead. We'll see, lol...</p><br>
+      <p>Also, new page out today! Be sure to check it out!</p><br>
+      
+      <p>Anyway, that's all folks! As always, thanks for reading!</p>
     `,
     tags: ["comic", "update"],
-    comicPage: 185,
+    comicPage: 188,
     featured: true
   },
 
   {
-    id: "sketchbook-thoughts",
-    title: "Sketchbook Thoughts",
-    date: "2026-03-28",
+    id: "website-upgrade",
+    title: "Welcome to Nocturne21.com!",
+    date: "2026-03-30",
     excerpt: "Some loose notes and visual experiments from this week.",
     content: `
-      <p>Another post...</p>
+      <p>Hey folks! I know this site's been around for a while now, but it's gone through a lot of changes and I haven't really taken the time to 
+      get it set up the way I want to. I feel like I'm finally at a place where I'm happy with how it looks and functions and am ready to show it off.
+      Keep in mind, I am <i>still</i> working on it, so things may break or change sometimes, as I continue to add on.</p><br>
+      <p>So, welcome to the new and improved site! I'll be posting new pages and blog entries on here for y'all to read. If you'd like to receive updates
+      you can subscribe via RSS by clicking the RSS icon in my share bar! That'll let you know whenever I post a new page or write in my blog. You can also
+      read about Nocturne's history on the "About" page, get to know the characters on "Cast" or see some fun extras like artwork from me and fanart by others!
+      </p><br>
+      <p>That being said, welcome again to the Nocturne 21 official site and thanks for reading!</p>
     `,
-    tags: ["art", "process"]
+    tags: ["art", "process", "nocturne 21", "comic", "graphic novel"]
   }
 ];
 
