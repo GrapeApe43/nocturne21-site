b054abdc714316d116b99657368eb5a8178adf77
GrapeApe43 <aprilferreroart@gmail.com>
2026-06-25 19:46:33 -0400

    Update comic_settings.js

--- js/comic_settings.js
+++ js/comic_settings.js
@@ -11,7 +11,7 @@ let pg = Number(findGetParameter("pg")); //make "pg" mean the current page numbe
 ////////////////////////
 
 //REALLY IMPORTANT ONES
-const maxpg = 195; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
+const maxpg = 196; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
 //YOU MUST UPDATE THIS NUMBER EVERY TIME YOU ADD A NEW PAGE or else it wont display the most recent page
 
 // COMIC PAGE SETTINGS
@@ -1826,6 +1826,18 @@ const pgData = [
     // NEW ↓↓↓
     description: "Page 179 of Nocturne 21: Why boys shouldn't play with sharp objects...",
     thumb: "https://nocturne21.com/img/thumbs/pg195.png"
+},
+             {
+    pgNum: 196,
+    title: "Page 180",
+    date: writeDate(2026, 06, 25),
+    altText: "",
+    imageFiles: 1,
+    authorNotes: `<p>Pretty sure this was EXACTLY what Yosh was afraid of. Kai, my guy, you need to learn some defense.</p>`,
+
+    // NEW ↓↓↓
+    description: "Page 180 of Nocturne 21",
+    thumb: "https://nocturne21.com/img/thumbs/pg196.png"
 },
 ];
 
