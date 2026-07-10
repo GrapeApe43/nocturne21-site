f2eb0eb3a0560de7b2f2c357c0dece8e8b25e0f7
GrapeApe43 <aprilferreroart@gmail.com>
2026-07-09 20:21:58 -0400

    Update comic_settings.js

--- js/comic_settings.js
+++ js/comic_settings.js
@@ -11,7 +11,7 @@ let pg = Number(findGetParameter("pg")); //make "pg" mean the current page numbe
 ////////////////////////
 
 //REALLY IMPORTANT ONES
-const maxpg = 197; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
+const maxpg = 198; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
 //YOU MUST UPDATE THIS NUMBER EVERY TIME YOU ADD A NEW PAGE or else it wont display the most recent page
 
 // COMIC PAGE SETTINGS
@@ -1852,6 +1852,18 @@ const pgData = [
     // NEW ↓↓↓
     description: "Page 181 of Nocturne 21",
     thumb: "https://nocturne21.com/img/thumbs/pg197.png"
+},
+             {
+    pgNum: 198,
+    title: "Page 182",
+    date: writeDate(2026, 07, 09),
+    altText: "",
+    imageFiles: 1,
+    authorNotes: `<p>I guess phone smashing runs in the family. Shame none of them own a nokia.</p>`,
+
+    // NEW ↓↓↓
+    description: "Page 182 of Nocturne 21",
+    thumb: "https://nocturne21.com/img/thumbs/pg198.png"
 },
 ];
 
